9f42518d878b6c11a9f405833bacc07b66cf064d
nomena <hei.nomenjanahary@gmail.com>
2026-01-16 16:28:41 +0300

    first commit

---
+++ src/main/java/hei/group/exercicetd3/ExerciceTd3Application.java
@@ -0,0 +1,13 @@
+package hei.group.exercicetd3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExerciceTd3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExerciceTd3Application.class, args);
+    }
+
+}
---
+++ src/test/java/hei/group/exercicetd3/ExerciceTd3ApplicationTests.java
@@ -0,0 +1,13 @@
+package hei.group.exercicetd3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExerciceTd3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
